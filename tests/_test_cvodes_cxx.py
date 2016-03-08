25fa26c58cc16951d302de8d73540bfa5d51d85d
Björn Dahlgren <bjodah@gmail.com>
2016-03-09 00:02:57 +0100

    Fix python bindings

--- tests/_test_cvodes_cxx.py
+++ tests/_test_cvodes_cxx.py
@@ -1,11 +1,11 @@
-import pyximport
-pyximport.install()
-
-from _cvodes_cxx import PyDecay
-
 from math import exp
 
-def test_PyDecay():
+
+if __name__ == '__main__':
+    import pyximport
+    pyximport.install()
+    from _cvodes_cxx import PyDecay
+
     pd = PyDecay(1.0)
     tout, yout = pd.adaptive(1.0, 1.0)
     for t, y in zip(tout, yout):
