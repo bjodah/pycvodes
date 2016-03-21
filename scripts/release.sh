cf3443b7fd1d6e59d02f779ed27f8efbe09b8461
Björn Dahlgren <bjodah@gmail.com>
2016-03-21 11:01:02 +0100

    Update README and release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,7 +8,9 @@ if [[ $1 != v* ]]; then
     echo "Argument does not start with 'v'"
     exit 1
 fi
-find . -type f -iname "*.pyc" -o -iname "*.o" -o -iname "*.so" -exec rm {} +
+find . -type f -iname "*.pyc" -exec rm {} +
+find . -type f -iname "*.o" -exec rm {} +
+find . -type f -iname "*.so" -exec rm {} +
 find . -type d -name "__pycache__" -exec rmdir {} +
 ./scripts/check_clean_repo_on_master.sh
 cd $(dirname $0)/..
@@ -34,6 +36,7 @@ if [[ -d dist/conda-recipe-$VERSION ]]; then
 fi
 cp -r conda-recipe/ dist/conda-recipe-$VERSION
 sed -i -E -e "s/version:(.+)/version: $VERSION/" -e "s/path:(.+)/fn: $PKG-$VERSION.tar.gz\n  url: https:\/\/pypi.python.org\/packages\/source\/${PKG:0:1}\/$PKG\/$PKG-$VERSION.tar.gz#md5=$MD5\n  md5: $MD5/" -e '/cython/d' dist/conda-recipe-$VERSION/meta.yaml
+env ${PKG_UPPER}_RELEASE_VERSION=$1 python setup.py upload_sphinx
 
 # Specific for this project (alternatively use a Docker image for this):
 SERVER=hera
@@ -43,4 +46,3 @@ for CONDA_PY in 27 34 35; do
     # This will test the the source distribution uploaded to PyPI:
     ssh $PKG@$SERVER "source /etc/profile; CONDA_PY=$CONDA_PY CONDA_NPY=110 conda-build ~/public_html/conda-recipes/conda-recipe-$VERSION/"
 done
-
