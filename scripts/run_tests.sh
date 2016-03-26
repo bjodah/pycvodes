79ac2cbdebc625785863634eaf5647188bbaeb7a
Björn Dahlgren <bjodah@gmail.com>
2016-03-26 11:52:36 +0100

    Updated CI and release scripts

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,8 +3,6 @@
 #   $ ./scripts/run_tests.sh
 # or
 #   $ ./scripts/run_tests.sh --cov pycvodes --cov-report html
-python2.7 setup.py build_ext -i
-python2.7 -m pytest
-python3 setup.py build_ext -i
-python3 -m pytest --doctest-modules --pep8 --flakes $@
-python3 -m doctest README.rst
+${PYTHON:-python} setup.py build_ext -i
+${PYTHON:-python} -m pytest --doctest-modules --pep8 --flakes $@
+${PYTHON:-python} -m doctest README.rst
