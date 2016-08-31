d8a4dc2f2b5a47e182376fada0122e3112d39bc7
Björn Dahlgren <bjodah@gmail.com>
2016-08-31 17:49:00 +0200

    ulimit -v causes segfault in unrelated libraries / programs

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,7 +3,6 @@
 #   $ ./scripts/run_tests.sh
 # or
 #   $ ./scripts/run_tests.sh --cov pycvodes --cov-report html
-ulimit -v 524288  # 512 MiB virt. mem: needed for testing MemoryError (std::bad_alloc)
 ${PYTHON:-python} setup.py build_ext -i
 ${PYTHON:-python} -m pytest --doctest-modules --pep8 --flakes $@
 ${PYTHON:-python} -m doctest README.rst
