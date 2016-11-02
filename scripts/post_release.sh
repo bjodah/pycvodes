cf05f0a816a7b1d224263a80d5fbfb8ceb3f1156
Björn Dahlgren <bjodah@gmail.com>
2016-11-02 20:01:53 +0100

    Prepare for v0.6.1 release

--- scripts/post_release.sh
+++ scripts/post_release.sh
@@ -19,8 +19,8 @@ if [[ -d "dist/conda-recipe-$VERSION" ]]; then
 fi
 cp -r conda-recipe/ dist/conda-recipe-$VERSION
 sed -i -E \
-    -e "s/\{\% set version(.+)/\{\% set version = \"$VERSION\" \%\}/" \
-    -e "s/git_url:(.+)/fn: $PKG-$VERSION.tar.gz\n  url: https:\/\/pypi.io\/packages\/source\/${PKG:0:1}\/${PKG}\/${PKG}-$VERSION.tar.gz\n  sha256: $SHA256/" \
+    -e "s/\{\% set version(.+)/\{\% set version = \"$VERSION\" \%\}\n\{\% set sha256 = \"$SHA256\" \%\}/" \
+    -e "s/git_url:(.+)/fn: \{\{ name \}\}-\{\{ version \}\}.tar.gz\n  url: https:\/\/pypi.io\/packages\/source\/\{\{ name\[0\] \}\}\/\{\{ name \}\}\/\{\{ name \}\}-\{\{ version \}\}.tar.gz\n  sha256: \{\{ sha256 \}\}/" \
     -e "/cython/d" \
     dist/conda-recipe-$VERSION/meta.yaml
 
