5f51b5c15ab3e942029fd3e978922b7da59039ee
Björn Dahlgren <bjodah@gmail.com>
2017-02-08 11:20:08 +0100

    openblas choice was not saved in conda distribution

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+sed -i -E -e 's/lapack/openblas/' pycvodes/_config.py
 PYCVODES_LAPACK=openblas CPLUS_INCLUDE_PATH=${PREFIX}/include ${PYTHON} setup.py build
 ${PYTHON} setup.py install --single-version-externally-managed --record record.txt
