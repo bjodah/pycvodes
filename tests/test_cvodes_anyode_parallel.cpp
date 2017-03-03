e05e39eb71ecf2d5fe992d15ce436d8f0f9c6571
Björn Dahlgren <bjodah@gmail.com>
2017-03-03 12:23:38 +0100

    Don't test OpenMP when using clang for now

--- tests/test_cvodes_anyode_parallel.cpp
+++ tests/test_cvodes_anyode_parallel.cpp
@@ -34,3 +34,40 @@ TEST_CASE( "decay_adaptive", "[multi_adaptive]" ) {
         REQUIRE( systems[idx]->last_integration_info_dbl["time_wall"] < 100 );
     }
 }
+
+TEST_CASE( "decay_predefined", "[multi_predefined]" ) {
+    std::vector<double> k {{ 2.0, 3.0}};
+    Decay odesys1(k[0]);
+    Decay odesys2(k[1]);
+    std::vector<Decay *> systems {{ &odesys1, &odesys2 }};
+    std::vector<double> y0 {{ 5.0, 7.0 }};
+    std::vector<double> yout(2*10);
+    std::vector<double> tout(2*10);
+
+    yout[0] = 1.0;
+    yout[10] = 1.0;
+    for (int i=0; i<2; ++i)
+        for (int j=0; j<10; ++j)
+            tout[10*i + j] = j/10.0;
+    const int mxsteps = 0;
+    std::vector<double> dx0 {{ 0, 0 }};
+    std::vector<double> dx_min {{ 0, 0 }};
+    std::vector<double> dx_max {{ 0, 0 }};
+
+    auto result = cvodes_anyode_parallel::multi_predefined(
+        systems, {1e-10}, 1e-10, cvodes_cxx::LMM::Adams, &y0[0], 10, &tout[0], &yout[0],
+        mxsteps, &dx0[0], &dx_min[0], &dx_max[0]);
+    REQUIRE(result.size() == 2);
+    REQUIRE(result[0].first == 10);
+    REQUIRE(result[1].first == 10);
+
+    for (int idx=0; idx<2; ++idx){
+        for (unsigned j=0; j<10; ++j){
+            REQUIRE( std::abs(y0[idx]*std::exp(tout[10*idx + 0]-tout[10*idx + j]) - yout[10*idx + j]) < 1e-8 );
+        }
+        REQUIRE( systems[idx]->last_integration_info["n_steps"] > 1 );
+        REQUIRE( systems[idx]->last_integration_info["n_steps"] < 997 );
+        REQUIRE( systems[idx]->last_integration_info_dbl["time_wall"] > 0 );
+        REQUIRE( systems[idx]->last_integration_info_dbl["time_wall"] < 100 );
+    }
+}
