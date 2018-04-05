f7a7acc9af1d5aa6c7eb4ac8464954884ae2cc30
Björn Dahlgren <bjodah@gmail.com>
2018-04-05 15:26:17 +0200

    Add conda-forge

--- .ci/download_and_install_miniconda3.sh
+++ .ci/download_and_install_miniconda3.sh
@@ -4,4 +4,8 @@ if [ -d "$PREFIX" ]; then 2>&1 echo "Directory already exists: $PREFIX"; exit 1;
 wget --quiet https://repo.continuum.io/miniconda/Miniconda3-4.4.10-Linux-x86_64.sh -O ~/miniconda.sh
 /bin/bash ~/miniconda.sh -b -p "$PREFIX"
 rm ~/miniconda.sh
-PATH="$PREFIX/bin:$PATH" conda install conda-build
+PATH="$PREFIX/bin:$PATH" conda config --set always_yes yes && \
+PATH="$PREFIX/bin:$PATH" conda config --set changeps1 no && \
+PATH="$PREFIX/bin:$PATH" conda config --set show_channel_urls True && \
+PATH="$PREFIX/bin:$PATH" conda config --add channels conda-forge && \
+PATH="$PREFIX/bin:$PATH" conda install --quiet conda-build
