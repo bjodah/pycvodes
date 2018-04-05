3ea25c90778efe9ad26198850c10e1c1f3ffb1f4
Björn Dahlgren <bjodah@gmail.com>
2018-04-05 15:14:08 +0200

    Invert mkdir logic for cache

--- .ci/download_and_install_miniconda3.sh
+++ .ci/download_and_install_miniconda3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 PREFIX=$1
-if [ ! -d "$PREFIX" ]; then 2>&1 echo "Not a directory: $PREFIX"; exit 1; fi
+if [ -d "$PREFIX" ]; then 2>&1 echo "Directory already exists: $PREFIX"; exit 1; fi
 wget --quiet https://repo.continuum.io/miniconda/Miniconda3-4.4.10-Linux-x86_64.sh -O ~/miniconda.sh
 /bin/bash ~/miniconda.sh -b -p "$PREFIX"
 rm ~/miniconda.sh
