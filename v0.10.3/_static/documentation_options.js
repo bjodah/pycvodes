72c8031f74c851bed45aa05d2c99ece0e2c57e49
Björn Dahlgren <bjodah@gmail.com>
2018-04-05 13:52:44 +0200

    Release docs for v0.10.3

---
+++ v0.10.3/_static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: '0.10.3',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
