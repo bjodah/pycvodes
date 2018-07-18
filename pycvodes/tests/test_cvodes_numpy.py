1d4caeba2ce138aeacb551af8d1751c6d99b5d86
Björn Dahlgren <bjodah@gmail.com>
2018-07-18 17:15:30 +0200

    Add debug statement

--- pycvodes/tests/test_cvodes_numpy.py
+++ pycvodes/tests/test_cvodes_numpy.py
@@ -656,6 +656,8 @@ def test_sparse_jac_predefined():
     from .._config import env
     if env['LAPACK'] in ('', '0'):
         pytest.skip("sparse jacobian tests require BLAS/LAPACK for KLU solver")
+    else:
+        print("env['LAPACK'] = %s" % env['LAPACK'])  # debug
     k = 2.0, 3.0, 4.0
     y0 = [0.7, 0.3, 0.5]
     xout = np.linspace(0, 3, 31)
