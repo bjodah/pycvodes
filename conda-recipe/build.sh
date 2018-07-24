7fa51c651fe083f439761e7a17e54962527a8a36
Björn Dahlgren <bjodah@gmail.com>
2018-07-24 19:27:21 +0200

    Update AnyODE (bjodah/anyode#19) (#82)
    * Update AnyODE (bjodah/anyode#19)
    
    * See if OS X linking error was due to conda-forge/sundials-feedstock#21
    
    * Build conda-recipe with sundials 2.7.0 for now
    
    * Update libraries in conda-recipe to match sundials 2.7.0
    
    * Switch back to sundials-3.1.1

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 export PYCVODES_LAPACK=openblas
+# Sundials 2.7:
+#export PYCVODES_SUNDIALS_LIBS=sundials_cvodes,sundials_nvecserial
+
+# Sundials 3.1:
 export PYCVODES_SUNDIALS_LIBS=sundials_cvodes,sundials_nvecserial,sundials_sunlinsollapackdense,sundials_sunlinsollapackband,sundials_sunlinsolklu,sundials_sunmatrixsparse
+
 cat <<EOF>pycvodes/_config.py
 env = {
     'LAPACK': "${PYCVODES_LAPACK}",
