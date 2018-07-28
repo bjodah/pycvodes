5d93b9e03c4d536aaafeb55f7938932ac839ca12
spcornelius <spcornelius@gmail.com>
2018-07-28 17:44:12 -0400

    Fix incorrect use of KLU config vars in builds/tests

--- pycvodes/tests/test_cvodes_numpy.py
+++ pycvodes/tests/test_cvodes_numpy.py
@@ -12,7 +12,8 @@ from pycvodes import (
 
 def requires_klu(_test):
     from .._config import env
-    return pytest.mark.skipif(not env['KLU'], reason="sparse jacobian tests require KLU to be enabled")(_test)
+    return pytest.mark.skipif(env.get('PYCVODES_NO_KLU', '0') == '1',
+                              reason="sparse jacobian tests require KLU to be enabled")(_test)
 
 
 def high_precision(_test):
