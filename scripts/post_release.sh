8d82350288f822526420fef6abe339591b624a6d
Björn Dahlgren <bjodah@gmail.com>
2018-10-10 18:04:23 +0200

    Tweak post release script wrt conda recipe render

--- scripts/post_release.sh
+++ scripts/post_release.sh
@@ -19,8 +19,9 @@ if [[ -d "dist/conda-recipe-$VERSION" ]]; then
 fi
 cp -r conda-recipe/ dist/conda-recipe-$VERSION
 sed -i -E \
-    -e "s/\{\% set version(.+)/\{\% set version = \"$VERSION\" \%\}\n\{\% set sha256 = \"$SHA256\" \%\}/" \
+    -e "s/version:(.+)/version: \"$VERSION\"/" \
     -e "s/git_url:(.+)/fn: \{\{ name \}\}-\{\{ version \}\}.tar.gz\n  url: https:\/\/pypi.io\/packages\/source\/\{\{ name\[0\] \}\}\/\{\{ name \}\}\/\{\{ name \}\}-\{\{ version \}\}.tar.gz\n  sha256: \{\{ sha256 \}\}/" \
+    -e "/set version/d" \
     -e "/cython/d" \
     dist/conda-recipe-$VERSION/meta.yaml
 
