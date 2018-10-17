2886fd1c48e5d62a2d58c9cea6242879c29ef723
Björn Dahlgren <bjodah@gmail.com>
2018-10-17 15:37:38 +0200

    More robust test_examples.py

--- examples/tests/test_examples.py
+++ examples/tests/test_examples.py
@@ -13,10 +13,9 @@ tests = glob.glob(os.path.join(os.path.dirname(__file__), '../*.py'))
 
 @pytest.mark.parametrize('pypath', tests)
 def test_examples(pypath):
-    py_exe = 'python3' if sys.version_info.major == 3 else 'python'
+    py_exe = 'python%d.%d' % sys.version_info[:2]
     p = subprocess.Popen([py_exe, pypath])
     assert p.wait() == 0  # SUCCESS==0
 
-    py_exe = 'python3' if sys.version_info.major == 3 else 'python'
     p = subprocess.Popen([py_exe, pypath, '--nt', '2'])
     assert p.wait() == 0  # SUCCESS==0
