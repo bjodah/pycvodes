df7f1519750d7ad133f485952509f6cd0a439e60
Björn Ingvar Dahlgren <bjodah@gmail.com>
2020-04-20 23:50:42 +0200

    use env

--- .ci/run_ci.sh
+++ .ci/run_ci.sh
@@ -22,7 +22,7 @@ python3 setup.py sdist
 if [[ "${LOW_PRECISION:-0}" != "1" ]]; then
     if [ -d build/ ]; then rm -r build/; fi
     CXX=clang++-10 CC=clang-10 CFLAGS="-fsanitize=address -DPYCVODES_CLIP_TO_CONSTRAINTS=1 $CFLAGS" python3 setup.py build_ext -i
-    export PYTHON="LD_PRELOAD=/usr/lib/llvm-10/lib/clang/10.0.0/lib/linux/libclang_rt.asan-x86_64.so ASAN_OPTIONS=abort_on_error=1,detect_leaks=0 python3" PYTHONPATH=$(pwd)
+    export PYTHON="env LD_PRELOAD=/usr/lib/llvm-10/lib/clang/10.0.0/lib/linux/libclang_rt.asan-x86_64.so ASAN_OPTIONS=abort_on_error=1,detect_leaks=0 python3" PYTHONPATH=$(pwd)
     ./scripts/run_tests.sh
     LINKLIBS=$(python3 -c "from pycvodes._libs import print_libs_linkline as pll; pll()")
     #unset LD_PRELOAD ASAN_OPTIONS
