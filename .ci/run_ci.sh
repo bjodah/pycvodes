3a694be56a95a50ac35d98bf5a3861eee03d36a7
Björn Ingvar Dahlgren <bjodah@gmail.com>
2024-04-05 13:52:28 +0200

    linking

--- .ci/run_ci.sh
+++ .ci/run_ci.sh
@@ -35,7 +35,7 @@ if [[ "${LOW_PRECISION:-0}" == "0" ]]; then
         CC=clang++ \
         CXX=clang++ \
         CFLAGS="-fsanitize=address -stdlib++-isystem ${LIBCXX_ASAN_ROOT}/include/c++/v1 -ferror-limit=5 -stdlib=libc++ -DPYCVODES_CLIP_TO_CONSTRAINTS=1 -UNDEBUG -O0 -g $CFLAGS" \
-        LDFLAGS="-fsanitize=address -Wl,-rpath,${LIBCXX_ASAN_ROOT}/lib -L${LIBCXX_ASAN_ROOT}/lib -lc++ -lc++abi -stdlib=libc++" \
+        LDFLAGS="-fsanitize=address -Wl,-rpath,${LIBCXX_ASAN_ROOT}/lib -L${LIBCXX_ASAN_ROOT}/lib -lc++ -lc++abi -stdlib=libc++ $LDFLAGS" \
         ${PYTHON:-python3} setup.py build_ext -i
     ORI_PYTHON="${PYTHON:-python3}"
     export PYTHON="env LD_PRELOAD=$(clang++ --print-file-name=libclang_rt.asan.so) ASAN_OPTIONS=abort_on_error=1,detect_leaks=0 ${PYTHON:-python3}"  # asan-$(uname -m).so
