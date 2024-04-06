bfe4ed547b82b13436449bd639867ee4193a8964
Björn Ingvar Dahlgren <bjodah@gmail.com>
2024-04-06 22:47:13 +0200

    tweaks

--- .ci/test-case.sh
+++ .ci/test-case.sh
@@ -6,7 +6,6 @@ show_help() {
     echo "--python <python-exe-path>"
 }
 NATIVE=0
-PYTHON=""
 TEST_ASAN=0
 while [ $# -gt 1 ]; do
     case $1 in
@@ -28,24 +27,18 @@ while [ $# -gt 1 ]; do
             exit 1
     esac
 done
-if [ $NATIVE -eq 1 -a ! -z $PYTHON ]; then
-    >&2 echo "Cannot specify both --python and --native"
-    exit 1
-elif [ $NATIVE -eq 0 -a -z $PYTHON ]; then
-    >&2 echo "Need to specify either --python or --native"
-    exit 1
-fi
 
 SUNDBASE=$1
-if [ ! -d "$SUNDBASE" ]; then >&2 echo "No such directory: $SUNDBASE"; exit 1; fi
+if [ ! -d "$SUNDBASE/include/sundials" ]; then >&2 echo "No such directory: $SUNDBASE"; exit 1; fi
 if [[ $SUNDBASE =~ *-extended || $SUNDBASE =~ *-single ]]; then
     export PYCVODES_NO_LAPACK=1 PYCVODES_NO_KLU=1
 fi
+LINKLIBS="$(${PYTHON:-python3} setup.py --print-linkline)"
+export CPATH=/usr/include/suitesparse  # include <klu.h>
 export CXXFLAGS="${CXXFLAGS:-} -isystem $SUNDBASE/include"
-export LDFLAGS="-Wl,--disable-new-dtags -Wl,-rpath,$SUNDBASE/lib -L$SUNDBASE/lib -lopenblas"
+export LDFLAGS="$LINKLIBS -Wl,--disable-new-dtags -Wl,-rpath,$SUNDBASE/lib -L$SUNDBASE/lib -lopenblas"
 export LD_LIBRARY_PATH=$(compgen -G "/opt-2/llvm-*/lib")
 
-LINKLIBS="$(${PYTHON:-python3} setup.py --print-linkline)"
 if [ $TEST_ASAN -eq 1 ]; then
     export CXX=clang++
     LIBCXX_ASAN_ROOT=$(compgen -G "/opt-2/libcxx*-asan")
@@ -55,7 +48,7 @@ if [ $TEST_ASAN -eq 1 ]; then
         exit 1
     fi
     export CXXFLAGS="$CXXFLAGS -fsanitize=address -stdlib++-isystem ${LIBCXX_ASAN_INCLUDE} -ferror-limit=5"
-    export LDFLAGS="$LINKLIBS ${LDFLAGS:-} -fsanitize=address -Wl,-rpath,${LIBCXX_ASAN_ROOT}/lib -L${LIBCXX_ASAN_ROOT}/lib -lc++ -lc++abi -stdlib=libc++"
+    export LDFLAGS="${LDFLAGS:-} -fsanitize=address -Wl,-rpath,${LIBCXX_ASAN_ROOT}/lib -L${LIBCXX_ASAN_ROOT}/lib -lc++ -lc++abi -stdlib=libc++"
     export LIBRARY_PATH="$LLVM_ROOT/lib:${LIBCXX_ASAN_ROOT}/lib:${LIBRARY_PATH:-}"
     export PYTHON="env LD_PRELOAD=$(clang++ --print-file-name=libclang_rt.asan.so) ASAN_OPTIONS=abort_on_error=1,detect_leaks=0 ${PYTHON:-python3}"
 else
@@ -69,8 +62,8 @@ fi
 
 if [ $NATIVE -eq 1 ]; then
     cd tests/
-    make
     make clean
+    make PYTHON=${PYTHON}
 else
     CC=$CXX CFLAGS=$CXXFLAGS $PYTHON setup.py build_ext -i
     #gdb -ex r -args
