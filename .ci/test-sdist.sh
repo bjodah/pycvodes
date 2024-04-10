e326291e0c4ede5086e0c10fa2211da52c567861
Björn Ingvar Dahlgren <bjodah@gmail.com>
2024-04-10 17:44:09 +0200

    tweak test script

--- .ci/test-sdist.sh
+++ .ci/test-sdist.sh
@@ -11,7 +11,7 @@ env \
     CFLAGS="-isystem $SUNDBASE/include -isystem /usr/include/suitesparse" \
     LDFLAGS="-Wl,--disable-new-dtags -Wl,-rpath,$SUNDBASE/lib -L$SUNDBASE/lib" \
     CC=gcc CXX=g++ pip install pycvodes-*.tar.gz
-pip install pytest-flakes pytest-cov matplotlib sphinx numpydoc
+pip install pytest-flakes pytest-cov matplotlib sphinx numpydoc sphinx-rtd-theme
 ../scripts/run_tests.sh --cov pycvodes --cov-report html
 ../scripts/coverage_badge.py htmlcov/ htmlcov/coverage.svg
 
