2bebbeeefbf93dabceda56908d831696bafa670d
Björn Ingvar Dahlgren <bjodah@gmail.com>
2024-04-10 17:06:09 +0200

    tweaks

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,5 +3,5 @@
 #   $ ./scripts/run_tests.sh
 # or
 #   $ ./scripts/run_tests.sh --cov pycvodes --cov-report html
-${PYTHON:-python3} -m pytest --doctest-modules --flakes $@
-MPLBACKEND=Agg ${PYTHON:-python3} -m doctest README.rst
+${PYTHON:-python3} -m pytest --pyargs pycvodes --doctest-modules --flakes $@
+MPLBACKEND=Agg ${PYTHON:-python3} -m doctest $(dirname $(realpath $0))/../README.rst
