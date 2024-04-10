03989bc3bc9a81ead8b9ebbbe358321d7cc56348
Björn Ingvar Dahlgren <bjodah@gmail.com>
2024-04-10 16:31:39 +0200

    tweaks

--- .ci/run-matrix.sh
+++ .ci/run-matrix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 set -euxo pipefail
 export PATH=/opt-2/gcc-13/bin:$PATH
-export CC=gcc-13
+export CC=gcc
 export CXX=g++
 #                                     .ci/test-case.sh --asan --python /opt-3/cpython-v3.11.9-asan/bin/python3 /opt-3/sundials-6.7.0-asan
 PYCVODES_NO_LAPACK=1 PYCVODES_NO_KLU=1 .ci/test-case.sh        --python /opt-3/cpython-v3.12.2-release/bin/python3 /opt-3/sundials-6.7.0-single
