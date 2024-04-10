01499f9a437384b55f703346bcc36019fefe0b0d
Björn Ingvar Dahlgren <bjodah@gmail.com>
2024-04-10 16:37:20 +0200

    tweak test-sdist

--- .ci/test-sdist.sh
+++ .ci/test-sdist.sh
@@ -6,7 +6,9 @@ source /opt-3/cpython-v3.11-apt-deb/bin/activate
 git clean -xfd
 python3 setup.py sdist
 cd ./dist/
-CC=gcc pip install pycvodes-*.tar.gz
+SUNDBASE=/opt-2/sundials-6.7.0-release
+CFLAGS="-isystem $SUNDBASE/include $CFLAGS" CC=gcc pip install pycvodes-*.tar.gz
+LDFLAGS="-Wl,--disable-new-dtags -Wl,-rpath,$SUNDBASE/lib -L$SUNDBASE/lib $LDFLAGS"
 pip install pytest-flakes pytest-cov
 ./scripts/run_tests.sh --cov pycvodes --cov-report html
 ./scripts/coverage_badge.py htmlcov/ htmlcov/coverage.svg
