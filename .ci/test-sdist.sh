f5b4fc847ad915597a8f0f9a4a6286d6b7b5c4c1
Björn Ingvar Dahlgren <bjodah@gmail.com>
2024-04-10 16:41:23 +0200

    teak test script

--- .ci/test-sdist.sh
+++ .ci/test-sdist.sh
@@ -6,7 +6,7 @@ source /opt-3/cpython-v3.11-apt-deb/bin/activate
 git clean -xfd
 python3 setup.py sdist
 cd ./dist/
-SUNDBASE=/opt-2/sundials-6.7.0-release
+SUNDBASE=/opt-3/sundials-6.7.0-release
 CFLAGS="-isystem $SUNDBASE/include" CC=gcc pip install pycvodes-*.tar.gz
 LDFLAGS="-Wl,--disable-new-dtags -Wl,-rpath,$SUNDBASE/lib -L$SUNDBASE/lib $LDFLAGS"
 pip install pytest-flakes pytest-cov
