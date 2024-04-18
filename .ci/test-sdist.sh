272deaddca74aa40206dac45fda86d6ba8cfcbd9
Björn Ingvar Dahlgren <bjodah@gmail.com>
2024-04-18 15:11:01 +0200

    pip install package before generate_docs?

--- .ci/test-sdist.sh
+++ .ci/test-sdist.sh
@@ -16,4 +16,5 @@ pip install pytest-flakes pytest-cov matplotlib sphinx numpydoc sphinx-rtd-theme
 ../scripts/coverage_badge.py htmlcov/ htmlcov/coverage.svg
 
 cd -
+pip install .
 ./scripts/generate_docs.sh
