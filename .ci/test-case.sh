3f25e39795a6dac8e8688db31bba8683a69fbff9
Bjorn <bjodah@gmail.com>
2025-07-24 17:22:07 +0200

    Bump ci 33 (#145)
    * CI: fix glob of python paths
    
    * Fix typo
    
    * typo2
    
    * tweaks
    
    * Handle asan detect_leaks
    
    * typo
    
    * Change how CXXFLAGS is set in Makefile
    
    * c++23
    
    * Explicitly opt-in to use numpy C-API 1.7+
    
    * add c++ standard library to LD_PRELOADED shared libs for pytetest
    
    * Complications with trying to keep LD_PRELOAD separate
    
    * try to use actual .so file (instead of what looks like a linkerscript)
    
    * Compilation: tweak paths and flags
    
    * Also preload libc++abi.so
    
    * Add libunwind too
    
    * also preload libasan
    
    * Tweak script for CI run with address sanitizer (asan)
    
    * Update paths
    
    * Newer setuptools actually use CXXFLAGS...

--- .ci/test-case.sh
+++ .ci/test-case.sh
@@ -44,7 +44,13 @@ if [ ! -d "$SUNDBASE/include/sundials" ]; then >&2 echo "No such directory: $SUN
 if [[ $SUNDBASE =~ *-extended || $SUNDBASE =~ *-single ]]; then
     export PYCVODES_NO_LAPACK=1 PYCVODES_NO_KLU=1
 fi
-LINKLIBS="$(${PYTHON:-python3} setup.py --print-linkline)"
+PYTHON=${PYTHON:-python3}
+if [ $TEST_ASAN -eq 1 ]; then
+    export PYTHON_ENV="env ASAN_OPTIONS=abort_on_error=1,detect_leaks=0"
+else
+    export PYTHON_ENV="env"
+fi
+LINKLIBS="$(${PYTHON_ENV} ${PYTHON} setup.py --print-linkline)"
 export CPATH=/usr/include/suitesparse  # include <klu.h>
 export CXXFLAGS="${CXXFLAGS:-} -isystem $SUNDBASE/include"
 export LDFLAGS="$LINKLIBS -Wl,--disable-new-dtags -Wl,-rpath,$SUNDBASE/lib -L$SUNDBASE/lib -lopenblas"
@@ -53,6 +59,8 @@ export LD_LIBRARY_PATH=$(compgen -G "/opt-2/llvm-*/lib")
 if [ $TEST_ASAN -eq 1 ]; then
     export CC=clang
     export CXX=clang++
+    LLVM_ROOT=$(compgen -G "/opt-2/llvm-??")
+    LLVM_LIB_DIR=$(compgen -G "${LLVM_ROOT}/lib/$(uname -m)-*")
     LIBCXX_ASAN_ROOT=$(compgen -G "/opt-2/libcxx*-asan")
     LIBCXX_ASAN_INCLUDE=${LIBCXX_ASAN_ROOT}/include/c++/v1
     if [ ! -d "$LIBCXX_ASAN_INCLUDE" ]; then
@@ -62,14 +70,17 @@ if [ $TEST_ASAN -eq 1 ]; then
     export CXXFLAGS="$CXXFLAGS -fsanitize=address -stdlib++-isystem ${LIBCXX_ASAN_INCLUDE} -ferror-limit=5"
     export LDFLAGS="${LDFLAGS:-} -fsanitize=address -Wl,-rpath,${LIBCXX_ASAN_ROOT}/lib -L${LIBCXX_ASAN_ROOT}/lib -lc++ -lc++abi -stdlib=libc++"
     export LIBRARY_PATH="$LLVM_ROOT/lib:${LIBCXX_ASAN_ROOT}/lib:${LIBRARY_PATH:-}"
-    # LD_PRELOAD=$(clang++ --print-file-name=libclang_rt.asan.so)
-    export PYTHON="env ASAN_OPTIONS=abort_on_error=1,detect_leaks=0 ${PYTHON:-python3}"
+    #LD_PRELOAD=
+
+    export PYTHON_ENV="$PYTHON_ENV LD_PRELOAD=$(clang++ --print-file-name=libclang_rt.asan.so):${LIBCXX_ASAN_ROOT}/lib/libc++.so.1.0:${LIBCXX_ASAN_ROOT}/lib/libc++abi.so.1.0:${LIBCXX_ASAN_ROOT}/lib/libunwind.so.1.0"  # Or this failure appears:
+    # AddressSanitizer: CHECK failed: asan_interceptors.cpp:463 "((__interception::real___cxa_throw)) != (0)" (0x0, 0x0)
+    OPENMP_LIB="-Wl,-rpath,${LLVM_LIB_DIR} -lomp"
 else
     export CC=gcc
     export CXX=g++
-    if $CXX --version | head -n 1 | grep -E 'g++\.*13\.[0-9]\.[0-9]$'; then exit 1; fi
     export CXXFLAGS="$CXXFLAGS -D_GLIBCXX_DEBUG -D_GLIBCXX_PEDANTIC"
     export CONTEXT="echo ''; echo ''; valgrind --error-exitcode=1"
+    OPENMP_LIB="-lgomp"
 fi
 
 
@@ -77,7 +88,7 @@ if [ -d ./build ]; then
     rm -r ./build
 fi
 
-CC=$CXX CFLAGS=$CXXFLAGS $PYTHON setup.py build_ext -i
+CC=$CXX CFLAGS="$CXXFLAGS -DNPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION" $PYTHON_ENV $PYTHON setup.py build_ext -i
 
 export PYTHONPATH=$(pwd)
 
@@ -96,7 +107,7 @@ fi
 # /opt-2/libcxx18-asan/lib/libc++abi.so:\
 # /opt-2/libcxx18-asan/lib/libunwind.so \
 #gdb -ex r -args
-$PYTHON -m pytest -v "$EXTRA_PYTEST_FLAGS"
+$PYTHON_ENV $PYTHON -m pytest -sv "$EXTRA_PYTEST_FLAGS"
 
 
 if [[ $SUNDBASE =~ .*-single ]]; then
@@ -104,6 +115,6 @@ if [[ $SUNDBASE =~ .*-single ]]; then
 else
     cd tests/
     make clean
-    make PYTHON=${PYTHON}
+    make PYTHON="$PYTHON_ENV ${PYTHON}" OPENMP_LIB="${OPENMP_LIB}"
     cd -
 fi
