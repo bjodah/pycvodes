0fafa4eb20febbc6b3dff80130d81e8d993740c0
Björn Ingvar Dahlgren <bjodah@gmail.com>
2025-07-24 16:49:15 +0200

    Update paths

--- .ci/test-sdist.sh
+++ .ci/test-sdist.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -euxo pipefail
-export PATH="/opt-2/gcc-13/bin:$PATH"
+#export PATH="/opt-2/gcc-13/bin:$PATH"
 
-source /opt-3/cpython-v3.11-apt-deb/bin/activate
+source /opt-3/cpython-v3.*-apt-deb/bin/activate
 git clean -xfd
 python3 setup.py sdist
 cd ./dist/
-SUNDBASE=/opt-3/sundials-6.7.0-release
+SUNDBASE=$(compgen -G "/opt-3/sundials-6.*-release")
 env \
     CFLAGS="-isystem $SUNDBASE/include -isystem /usr/include/suitesparse" \
     LDFLAGS="-Wl,--disable-new-dtags -Wl,-rpath,$SUNDBASE/lib -L$SUNDBASE/lib" \
