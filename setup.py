56c514f9fb166964043896c71c144cee04f2b42a
Björn Ingvar Dahlgren <bjodah@gmail.com>
2025-09-09 12:36:25 +0200

    Revert "progress on scikit-build based setup"
    This reverts commit 53444ebbdb1dd4a41823b9cad182f128e955bef7.

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from skbuild import setup  # This line replaces 'from setuptools import setup'
-from setuptools import find_packages
-
 setup(
     name="pycvodes",
     version="0.14.7",
     description="A python wrapper around SUNDIALS' CVODES",
     author='Björn Dahlgren',
     license="BSD-2-Clause",
-    packages=find_packages(),
+    packages=['pycvodes'],
     python_requires=">=3.8",
     install_requires=["numpy"],
 )
