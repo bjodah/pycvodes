53444ebbdb1dd4a41823b9cad182f128e955bef7
Björn Ingvar Dahlgren <bjodah@gmail.com>
2025-09-09 11:56:52 +0200

    progress on scikit-build based setup

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from skbuild import setup  # This line replaces 'from setuptools import setup'
+from setuptools import find_packages
+
 setup(
     name="pycvodes",
     version="0.14.7",
     description="A python wrapper around SUNDIALS' CVODES",
     author='Björn Dahlgren',
     license="BSD-2-Clause",
-    packages=['pycvodes'],
+    packages=find_packages(),
     python_requires=">=3.8",
     install_requires=["numpy"],
 )
